78f34124e1dbbb2d0f2e21999cf600ed6c93a7af
4nglp <4nglpw@gmail.com>
2025-12-09 16:56:10 +0100

    td_2: ex_7

--- td_2/ex_7.py
+++ td_2/ex_7.py
@@ -1,7 +1,6 @@
-t = [1,1,2,1,1]
+t = [1,3,2,1,1]
 rev = []
 l = len(t)
-
 while l >= 0:
     rev.append(t[l-1])
     l-=1
@@ -11,3 +10,14 @@ for i in range(len(t)):
     if t[i] != rev[i]:
         sym = False 
 print(sym)
+
+s = True
+b = len(t)
+a = []
+for i in range(b):
+    a.append(t[b-1])
+    if t[i] != a[i]:
+        s = False
+    b-=1
+print(a)
+print(s)
