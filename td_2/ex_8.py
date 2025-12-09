2a2009f9e728094993188a36d99dbf81cc4935ca
4nglp <4nglpw@gmail.com>
2025-12-09 17:01:08 +0100

    td_2: ex_8 corrected

--- td_2/ex_8.py
+++ td_2/ex_8.py
@@ -1,8 +1,13 @@
 import numpy as np
 
-m1 = np.array([1,2,3,4,5,6,7,8,9])
-m2 = np.array([8,-1,8,2,1,3,18,2,32])
+m1 = np.array([
+    [1,2,3],[4,5,6],[7,8,9]
+])
+print(m1)
+m2 = np.array([
+    [8,-1,8],[2,1,3],[18,2,32]
+])
 add = m1 + m2
-mul = m1 * m2
+mul = m1 @ m2
 m2_tr = m2.T
-col2_mul =mul[1]
+col2_mul =mul[:,1]
