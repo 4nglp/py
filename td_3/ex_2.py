cba0a38f0ca467a6e67b2f0073eb6034709895a4
4nglp <4nglpw@gmail.com>
2026-01-17 00:02:09 +0100

    td_3: ex_2

---
+++ td_3/ex_2.py
@@ -0,0 +1,10 @@
+data = {
+    "first_name":"yahya",
+    "last_name":"aatii",
+    "age":41
+}
+data["last_name"]="rami"
+print("keys: ",data.keys())
+print("vals: ",data.values())
+print(data.items())
+print(f'{data["first_name"]} {data["last_name"]} is {data["age"]} years old')
