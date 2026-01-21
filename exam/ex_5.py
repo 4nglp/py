11035f7018cc2b573af7d026b598caafb940f956
4nglp <4nglpw@gmail.com>
2026-01-21 19:29:38 +0100

    exam: ex_5 updated

--- exam/ex_5.py
+++ exam/ex_5.py
@@ -5,19 +5,27 @@ m = np.array([
     [0.2,0.8,0],
     [0.3,0.3,0.4]
 ]) 
-rows, cols = m.shape
 
 def is_sato(p):
    is_valid = np.all((p >=0) & (p <= 1)) 
-   sum_row = np.all(np.sum(p, axis =1) == 1)
-   return is_valid,sum_row
+   sum_row = np.all(np.sum(p, axis = 1))   
+   return is_valid & sum_row
 print(is_sato(m))
 
 def is_bisto(p):
     is_valid = np.all((p >= 0) & (p <= 1))
     sum_row = np.all(np.sum(p,axis=1) == 1)
     sum_cols = np.all(np.sum(p,axis=0) == 1)
-    return is_valid,sum_row,sum_cols
+    return is_valid & sum_row & sum_cols
 print(is_bisto(m))
 
+n = [
+   [0.95,0.05],
+   [0.03,0.97]
+]
+t = [0.375,0.625]
 
+def stable_vec(g,h):
+    w = np.arary(h)@np.array(g)
+    return np.array_equal(w,h)
+print(stable_vec(n,t))
